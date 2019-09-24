16949fade856a9b53044c4abad395461c0acc3a2
Xian Zhang <xian.z512@gmail.com>
2019-09-24 11:07:21 -0400

    enable auto resize canvas

--- src/components/background/Synap.jsx
+++ src/components/background/Synap.jsx
@@ -6,88 +6,124 @@ import SynapGraph from "./SynapGraph"
 
 function Synap({ style }) {
   const refContainer = useRef(null)
+
   useLayoutEffect(() => {
     // The application will create a canvas element
-    const app = new PIXI.Application({
+    let app = new PIXI.Application({
       transparent: true,
       resizeTo: refContainer.current,
+      antialias: true,
+      autoStart: true,
     })
 
     refContainer.current.appendChild(app.view)
 
-    app.loader.load((loader, resources) => {
-      const color = 0x787878
-      const opacity = 1
-      const lineWidth = 1
-      const circleRadius = 4
-      const graphicNodes = []
-      const graphicEdges = []
+    let tickerCallback = null
 
-      const dotContainer = new PIXI.Container()
+    let canvasSize = [app.view.width, app.view.height]
 
-      const lineContainer = new PIXI.Container()
-
-      // draw line first then dots
-      app.stage.addChild(lineContainer)
-      app.stage.addChild(dotContainer)
-
-      const graph = new SynapGraph(100, app.view)
-      graph.generateGraph()
-
-      graph.nodes.forEach(e => {
-        const graphicNode = new PIXI.Graphics()
-
-        graphicNodes.push(graphicNode)
-
-        graphicNode.beginFill(color, opacity)
-        graphicNode.lineStyle(2, 0xffffff, 1, 1)
-        graphicNode.drawCircle(0, 0, circleRadius)
-        graphicNode.endFill()
-
-        // add to container
-        ;[graphicNode.x, graphicNode.y] = e
-
-        dotContainer.addChild(graphicNode)
+    function resetApp() {
+      // to destroy ticker, need to detach the old ticker before destory to avoid app re-trigger destroy call
+      app.ticker.remove(tickerCallback)
+      app.stage.removeChildren().forEach(child => {
+        child.destroy()
       })
+    }
 
-      graph.edges.forEach(e => {
-        const graphicEdge = new PIXI.Graphics()
-        graphicEdges.push(graphicEdge)
-        graphicEdge.lineStyle(lineWidth, color, opacity, 0.5, true)
-        graphicEdge.moveTo(...e[0])
-        graphicEdge.lineTo(...e[1])
-        lineContainer.addChild(graphicEdge)
-      })
+    const loadApp = () => {
+      app.loader.load((loader, resources) => {
+        const color = 0x787878
+        const opacity = 1
+        const lineWidth = 1
+        const circleRadius = 4
+        const graphicNodes = []
+        const graphicEdges = []
 
-      const speed = 0.2
-      app.ticker.add(deltaTime => {
-        for (let i = 0; i < graphicNodes.length; i += 1) {
-          const graphicNode = graphicNodes[i]
+        const dotContainer = new PIXI.Container()
+        const lineContainer = new PIXI.Container()
 
-          const movingDistance = speed * deltaTime
-          graph.updateNode(i, movingDistance)
-          const node = graph.nodes[i]
-          ;[graphicNode.x, graphicNode.y] = node
-        }
+        // draw line first then dots
+        app.stage.addChild(lineContainer)
+        app.stage.addChild(dotContainer)
 
-        for (let i = 0; i < graphicEdges.length; i += 1) {
-          const edge = graph.edges[i]
-          const graphicEdge = graphicEdges[i]
-          graphicEdge.clear()
+        const graph = new SynapGraph(100, app.view)
+        graph.generateGraph()
+
+        graph.nodes.forEach(e => {
+          const graphicNode = new PIXI.Graphics()
+
+          graphicNodes.push(graphicNode)
+
+          graphicNode.beginFill(color, opacity)
+          graphicNode.lineStyle(2, 0xffffff, 1, 1)
+          graphicNode.drawCircle(0, 0, circleRadius)
+          graphicNode.endFill()
+          ;[graphicNode.x, graphicNode.y] = e
+
+          dotContainer.addChild(graphicNode)
+        })
+
+        graph.edges.forEach(e => {
+          const graphicEdge = new PIXI.Graphics()
+          graphicEdges.push(graphicEdge)
           graphicEdge.lineStyle(lineWidth, color, opacity, 0.5, true)
-          graphicEdge.moveTo(...edge[0])
-          graphicEdge.lineTo(...edge[1])
+          graphicEdge.moveTo(...e[0])
+          graphicEdge.lineTo(...e[1])
+          lineContainer.addChild(graphicEdge)
+        })
 
-          graphicEdge.endFill()
+        const speed = 0.2
+        tickerCallback = deltaTime => {
+          for (let i = 0; i < graphicNodes.length; i += 1) {
+            const graphicNode = graphicNodes[i]
+
+            const movingDistance = speed * deltaTime
+            graph.updateNode(i, movingDistance)
+            const node = graph.nodes[i]
+            ;[graphicNode.x, graphicNode.y] = node
+          }
+
+          for (let i = 0; i < graphicEdges.length; i += 1) {
+            const edge = graph.edges[i]
+            const graphicEdge = graphicEdges[i]
+            graphicEdge.clear()
+            graphicEdge.lineStyle(lineWidth, color, opacity, 0.5, true)
+            graphicEdge.moveTo(...edge[0])
+            graphicEdge.lineTo(...edge[1])
+
+            graphicEdge.endFill()
+          }
         }
+
+        app.ticker.add(tickerCallback)
       })
-    })
+    }
+    loadApp()
+
+    function onResize() {
+      const currentCanvasSize = [app.view.width, app.view.height]
+      if (
+        currentCanvasSize[0] <= canvasSize[0] &&
+        currentCanvasSize[1] <= canvasSize[1]
+      ) {
+        return
+      }
+      canvasSize = currentCanvasSize
+      resetApp()
+      loadApp()
+    }
+    window.addEventListener("resize", onResize)
 
     return () => {
-      refContainer.current.removeChild(app.view)
-      app.destroy()
+      app.destroy(true)
+      refContainer.current.childNodes.forEach(child => {
+        refContainer.current.removeChild(child)
+      })
+      app = null
+      window.removeEventListener("resize", onResize)
     }
   }, [])
+
   return (
     <div
       ref={refContainer}
