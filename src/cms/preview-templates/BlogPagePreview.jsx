0c2e20e5341fe49a2e9cedefec4e6fc9b9fffc97
Xian Zhang <xian.z512@gmail.com>
2019-10-15 12:12:06 -0400

    prevent treeshaking plugins

--- src/cms/preview-templates/BlogPagePreview.jsx
+++ src/cms/preview-templates/BlogPagePreview.jsx
@@ -3,7 +3,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { BlogPageTemplate } from "../../templates/BlogPage"
 
-const ProductPagePreview = ({ entry, getAsset }) => {
+const ProductPagePreview = ({ entry }) => {
   const jumbotronProps = entry.getIn(["data", "jumbotron"])
 
   return <BlogPageTemplate jumbotronProps={jumbotronProps} isPreview />
@@ -13,7 +13,6 @@ ProductPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  getAsset: PropTypes.func,
 }
 
 export default ProductPagePreview
