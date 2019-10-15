470d9d59d92cf3b28a6bfabfcd5d70459fc61de7
Xian Zhang <xian.z512@gmail.com>
2019-10-15 11:27:29 -0400

    rename files

--- src/templates/AboutPage.jsx
+++ src/templates/AboutPage.jsx
@@ -2,7 +2,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import SEO from "../components/Seo"
+import SEO from "../components/SEO"
 import HeaderContainer from "../components/header/HeaderContainer"
 
 import BubbleTank from "../components/bubbles/BubbleTank"
