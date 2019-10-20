2a2d20070e1242cc3267292a6d3b77dc99ee3d21
Xian Zhang <xian.z512@gmail.com>
2019-10-20 15:23:34 -0400

    set icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,13 +67,13 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "Xian's Page",
+        short_name: "Xian",
         start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
+        background_color: "white",
+        theme_color: "#cc1f41",
         display: "minimal-ui",
-        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
