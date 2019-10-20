8aa276503eb6bbc3a117473a11d616d323a38c8e
Xian Zhang <xian.z512@gmail.com>
2019-10-20 14:46:51 -0400

    nothing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,6 +13,6 @@ export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (typeof window.IntersectionObserver === `undefined`) {
     import(`intersection-observer`)
-    console.log(`# IntersectionObserver is polyfilled!`)
+    // console.log(`# IntersectionObserver is polyfilled!`)
   }
 }
