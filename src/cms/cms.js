805ee3d93446fcdd7818abe4eedf0ecea9342696
Xian Zhang <xian.z512@gmail.com>
2019-10-22 22:58:55 -0400

    fix editor component for html image

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -25,24 +25,26 @@ CMS.registerEditorComponent({
   // Fields the user need to fill out when adding an instance of the component
   fields: [
     { name: "src", label: "src", widget: "image" },
-    { name: "containerStyle", label: "containerStyle", widget: "string" },
+    { name: "containerStyle", label: "Container Style", widget: "string" },
   ],
   // Pattern to identify a block as being an instance of this component
   pattern: /^<div[^<>]*?class='figureImage'[^<>]*?>.*?<\/div>$/,
   // Function to extract data elements from the regexp match
   fromBlock(match) {
     return {
-      src: match[0].match(/(?<=src=['"]).*?(?=['"])/),
-      containerStyle: match[0].match(/(?<=style=['"]).*?(?=['"])/),
+      src: match[0].match(/(?<=<img[^<>]*?src=['"]).*?(?=['"][^<>]*?>)/),
+      containerStyle: match[0].match(
+        /(?<=<div[^<>]*?style=['"]).*?(?=['"][^<>]*?>)/
+      ),
     }
   },
   // Function to create a text block from an instance of this component
   toBlock(obj) {
-    return `<div class='figureImage' style="${obj.style}" > <img src="${obj.src}"/> </div>`
+    return `<div class='figureImage' style="${obj.containerStyle}" > <img src="${obj.src}"/> </div>`
   },
   // Preview output for this component. Can either be a string or a React component
   // (component gives better render performance)
   toPreview(obj) {
-    return `<div class='figureImage' style="${obj.style}"> <img src="${obj.src}" style="width:100%;object-fit:cover;" /> </div>`
+    return `<div class='figureImage' style="${obj.containerStyle}"> <img src="${obj.src}" style="width:100%;object-fit:cover;" /> </div>`
   },
 })
