de71210ad3886740497474cfd53ce0d20caf22a4
Xian Zhang <xian.z512@gmail.com>
2020-11-25 17:19:55 -0500

    feat: add sentence embeddging model for FAQ style

--- src/chatbot/main.py
+++ src/chatbot/main.py
@@ -1,25 +1,31 @@
+from functools import singledispatch
+import numpy as np
+from haystack.document_store.sql import ORMBase
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.pool import SingletonThreadPool
+# import time
 import os
-from elasticsearch import Elasticsearch
-from subprocess import Popen, PIPE, STDOUT
+# from elasticsearch import Elasticsearch
+# from subprocess import Popen, PIPE, STDOUT
 from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
 from haystack.retriever.dense import EmbeddingRetriever
 import yaml
 from haystack import Finder
 from haystack.reader.farm import FARMReader
-from haystack.document_store.sql import SQLDocumentStore
-from haystack.retriever.sparse import TfidfRetriever
+from haystack.document_store.faiss import FAISSDocumentStore
+# from haystack.retriever.sparse import TfidfRetriever
 
-# if(prob<0.9) { add first then concat all top sol > 0.6}
 import uvicorn
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+# from starlette.middleware.cors import CORSMiddleware
 
-from subprocess import Popen, PIPE, STDOUT
-es_server = Popen(['elasticsearch-7.9.2/bin/elasticsearch'],
-                  stdout=PIPE, stderr=STDOUT,
-                  preexec_fn=lambda: os.setuid(1)  # as daemon
-                  )
+# from subprocess import Popen, PIPE, STDOUT
+# es_server = Popen(['elasticsearch-7.9.2/bin/elasticsearch'],
+#                   stdout=PIPE, stderr=STDOUT,
+#                   preexec_fn=lambda: os.setuid(1)  # as daemon
+#                   )
 
 
 app = FastAPI()
@@ -48,11 +54,30 @@ with open(configFile) as file:
 
 modelDir = config["modelDir"]
 sqlUrl = config["sqlUrl"]
-
+sqlUrlFAQ = config["sqlUrlFAQ"]
 
 # prediction = finder2.get_answers_via_similar_questions(
 #     question="How is the virus spreading?", top_k_retriever=1)
-import time
+
+# thread issue for sql :engine=create_engine('sqlite:///data.db', echo=True, connect_args={"check_same_thread": False})
+# or need to create new store instance each request
+# or# maintain the same connection per thread
+# from sqlalchemy.pool import SingletonThreadPool
+# engine = create_engine('sqlite:///mydb.db',
+#                 poolclass=SingletonThreadPool)
+
+
+@singledispatch
+def to_serializable(val):
+    """Used by default."""
+    return str(val)
+
+
+@to_serializable.register(np.float32)
+def ts_float32(val):
+    """Used if *val* is an instance of numpy.float32."""
+    return np.float64(val)
+
 
 class Chatbot:
     def __init__(self):
@@ -65,42 +90,93 @@ class Chatbot:
         self.retriever2 = None
         self.finder2 = None
 
-    def waitEsReady(self):
-        done = False
-        client = Elasticsearch(timeout=1000)
-        while not done:
-            try:
-                client.info()
-                done = True
-            except:
-                time.sleep(2)
-                pass
+    def initSql(self, url, document_store):
+        document_store.session.close()
+        engine = create_engine(url,
+                               poolclass=SingletonThreadPool, connect_args={"check_same_thread": False})
+        ORMBase.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        document_store.session = Session()
 
     def load(self):
-        chatbot.waitEsReady()
+        if(self.finder and self.finder2):
+            return
         if(not self.document_store2):
-            self.document_store2 = ElasticsearchDocumentStore(host="localhost", username="", password="",
-                                                              index="document",
-                                                              embedding_field="question_emb",
-                                                              embedding_dim=768,
-                                                              excluded_meta_data=["question_emb"])
+            self.document_store2 = FAISSDocumentStore.load(
+                sql_url=sqlUrlFAQ, faiss_file_path='faiss2')  # save before load in preprocess
+            self.initSql(url=sqlUrlFAQ, document_store=self.document_store2)
+        # else:  # reset session
+        #     # self.document_store2.session.close()
+        #     super(
+        #         FAISSDocumentStore, self.document_store2).__init__(url=sqlUrlFAQ)
         if(not self.retriever2):
             self.retriever2 = EmbeddingRetriever(document_store=self.document_store2,
                                                  embedding_model="sentence_bert-saved", use_gpu=False)
         if(not self.finder2):
             self.finder2 = Finder(reader=None, retriever=self.retriever2)
 
-        if(self.finder):
-            return
-        self.document_store = SQLDocumentStore(
-            url=sqlUrl) if not self.document_store else self.document_store
-        self.retriever = TfidfRetriever(
-            document_store=self.document_store) if not self.retriever else self.retriever
+        if(not self.document_store):
+            self.document_store = FAISSDocumentStore.load(faiss_file_path='faiss1',
+                                                          sql_url=sqlUrl)
+            self.initSql(url=sqlUrl, document_store=self.document_store)
+        # else:  # reset session
+        #     # self.document_store.session.close()
+        #     super(
+        #         FAISSDocumentStore, self.document_store).__init__(url=sqlUrl)
+        # self.retriever = EmbeddingRetriever( #redice load by sharing the same retriever and set store on fly??
+        #     document_store=self.document_store, embedding_model="sentence_bert-saved", use_gpu=False) if not self.retriever else self.retriever
+        if(not self.retriever):
+            self.retriever = self.retriever2
         self.reader = FARMReader(model_name_or_path=modelDir,
                                  use_gpu=False, no_ans_boost=0) if not self.reader else self.reader
         # reader = TransformersReader(model_name_or_path="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased", use_gpu=-1)
         self.finder = Finder(
             self.reader, self.retriever) if not self.finder else self.finder
+
+    def getfinder1(self):
+        self.retriever.document_store = self.document_store
+        return self.finder
+
+    def getfinder2(self):
+        self.retriever2.document_store = self.document_store2
+        return self.finder2
+
+    def endSessions(self):
+        self.document_store2.session.close()
+        self.document_store.session.close()
+
+    def get_answers_via_similar_questions(self, question, finder=None, top_k_retriever=1, filters=None, index=None):
+        if(not finder):
+            finder = self.finder2
+
+        if finder.retriever is None:
+            raise AttributeError(
+                "Finder.get_answers_via_similar_questions requires self.retriever")
+
+        results = {"question": question, "answers": []}  # type: Dict[str, Any]
+
+        # 1) Apply retriever to match similar questions via cosine similarity of embeddings
+        documents = finder.retriever.retrieve(
+            question, top_k=top_k_retriever, filters=filters, index=index)
+
+        # 2) Format response
+        for doc in documents:
+            # TODO proper calibratation of pseudo probabilities
+            cur_answer = {
+                "question": doc.text,
+                "answer": doc.meta["answer"],
+                # "document_id": doc.id,
+                # "context": doc.text,
+                "score": doc.score,
+                "probability": doc.probability/100,
+                # "offset_start": 0,
+                # "offset_end": len(doc.text),
+                # "meta": doc.meta
+            }
+
+            results["answers"].append(cur_answer)
+
+        return results
 
 
 chatbot = Chatbot()
@@ -117,17 +193,26 @@ def hi():
 
 @app.get("/qa/{question}")
 def qa(question: str):
-    if not chatbot.finder:
-        chatbot.load()
-
-    results2 = chatbot.finder2.get_answers_via_similar_questions(
-        question, top_k_retriever=1)
-    if(results2.answers[0].answer.probabilty >= 80):
-        return {"results": results2}
+    # if not chatbot.finder:
+    chatbot.load()
+    ret = [{"results": {"answers": []}}]
+    results2 = chatbot.get_answers_via_similar_questions(
+        question=question, finder=chatbot.getfinder2(), top_k_retriever=1)
+    if(len(results2["answers"]) > 0 and results2["answers"][0]["probability"] >= 0.8):
+        ret = {"results": results2}
     else:
-        results = chatbot.finder.get_answers(
+        results = chatbot.getfinder1().get_answers(
             question=question, top_k_retriever=2, top_k_reader=2)
-        return {"results": {"answers": results2["answers"]+results["answers"]}}
+        ret = {"results": {
+            "answers": results2["answers"]+results["answers"]}}
+        # ret = {"results": results}
+    # chatbot.endSessions()
+    # print(ret)
+    ans=[]
+    for item in ret["results"]["answers"]:
+        ans.append({"answer": item["answer"], "probability": item["probability"]})
+    # print(ans)
+    return {"results": {"answers": ans}}
 
 
 if __name__ == "__main__":
