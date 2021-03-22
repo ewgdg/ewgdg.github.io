211b62eff9539956aa175906684e58952407d64c
Xian Zhang <xian.z512@gmail.com>
2021-03-21 21:36:29 -0400

    fix typo

--- src/components/header/Jumbotron.jsx
+++ src/components/header/Jumbotron.jsx
@@ -2,7 +2,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import BackgroundImage from "gatsby-background-image"
-
+import _JSXStyle from "styled-jsx/style"
 const Jumbotron = ({
   image,
   headline,
