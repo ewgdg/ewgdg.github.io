d1e9cd5820f3f835054572331d675ebde19aa68b
Xian Zhang <xian.z512@gmail.com>
2024-07-14 23:49:14 -0700

    feat: bubble tank segmented to sections

--- src/components/bubbles/BubbleTank.jsx
+++ src/components/bubbles/BubbleTank.jsx
@@ -9,13 +9,16 @@ import ListSubheader from "@material-ui/core/ListSubheader"
 import { debounce } from "../../utils/throttle"
 import Bubble from "./Bubble"
 import ParallaxSection from "../sections/ParallaxSection"
+import Section from "../pageScroll/Section"
+import { Create } from "@material-ui/icons"
+import PageContainer, { SectionTypes } from "../pageScroll/Container"
 
 const useStyles = makeStyles({
   gridList: {
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
     // transform: "translateZ(0)",
     pointerEvents: "none",
-    overflow: "hidden",
+    overflow: "visible",
     backgroundColor: "rgba(12, 164, 255, 0.3)",
   },
 })
@@ -37,122 +40,114 @@ const useTileStyles = makeStyles({
 })
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i -= 1) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[array[i], array[j]] = [array[j], array[i]]
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
-function useInitCells(dataSize, cellsPerRow) {
-  const willMount = useRef(true)
 
-  // useEffect is called after component is mounted
-  useEffect(() => {
-    willMount.current = false
-    return () => {
-      willMount.current = true
-    }
-  }, [dataSize, cellsPerRow])
-
-  // expect to have 1.5 bubbles per row
-  const prob = 1.5 / cellsPerRow
-  const cells = useRef(null)
-  if (willMount.current) {
-    cells.current = []
-    // init
-    let currentIndex = 0
-    while (currentIndex < dataSize) {
-      if (Math.random() < prob) {
-        cells.current.push(currentIndex)
-        currentIndex += 1
-      } else {
-        cells.current.push(-1)
-      }
-    }
-  }
-  return cells.current
-}
 function random(min, max, isInteger = true) {
   let res = Math.random() * (max - min + (isInteger ? 1 : 0))
   if (isInteger) res = Math.floor(res)
   return res + min
 }
 
-function useInitBubbles(dataSize, cellHeight, cellsPerRow) {
-  const [isMounted, setIsMounted] = useState(false)
-  const [shouldInit, setShouldInit] = useState(false)
+function initBubbles(dataSize, cellHeight, cellsPerRow) {
+  const colors = [
+    "Violet",
+    "Aqua",
+    "Orange",
+    "skyblue",
+    "SlateBlue",
+    "Gray",
+    "gold",
+    "pink",
+    "#e91e63",
+    "#e91e63",
+    "#ff9e80",
+    "#1de9b6",
+    "#c2185b",
+    "#8bc34a",
+    "#b39ddb",
+    "DodgerBlue",
+    "Tomato",
+    "Orange",
+    "MediumSeaGreen",
+  ]
+  // init
+  const cellWidth = window.innerWidth / cellsPerRow
+  cellHeight = Math.min(cellWidth * 1.5, cellHeight)
+  const viewportHeight = window.innerHeight
+  const cellsPerCol = Math.max(2, Math.round(viewportHeight / cellHeight))
+  cellHeight = viewportHeight / cellsPerCol
 
-  // useEffect is called after component is mounted
-  useEffect(() => {
-    setIsMounted(true)
-    return () => {
-      setIsMounted(false)
+  const generateBubble = (pivotX, pivotY) => {
+    const maxRadius = Math.min(cellHeight, cellWidth) / 1.5
+    const radius = random(maxRadius / 2, maxRadius, false)
+    const bubbleColor = colors[random(0, colors.length - 1)]
+    let cols = 1
+    let rows = 1
+    if (radius * 2 > cellHeight) {
+      rows = 2
     }
-  }, [dataSize, cellsPerRow])
-
-  const bubbles = useRef(null)
-  if (!isMounted || shouldInit) {
-    if (shouldInit) {
-      setShouldInit(false)
+    if (radius * 2 > cellWidth) {
+      cols = 2
     }
-    const colors = [
-      "Violet",
-      "Aqua",
-      "Orange",
-      "skyblue",
-      "SlateBlue",
-      "Gray",
-      "gold",
-      "pink",
-      "#e91e63",
-      "#e91e63",
-      "#ff9e80",
-      "#1de9b6",
-      "#c2185b",
-      "#8bc34a",
-      "#b39ddb",
-      "DodgerBlue",
-      "Tomato",
-      "Orange",
-      "MediumSeaGreen",
-    ]
-    bubbles.current = []
-    // init
-    const cellWidth = window.innerWidth / cellsPerRow
-    cellHeight = Math.min(cellWidth * 1.5, cellHeight)
-    for (let i = 0; i < dataSize; i += 1) {
-      const maxRadius = Math.min(cellHeight, cellWidth) / 1.5
-      const radius = random(maxRadius / 2, maxRadius, false)
-      const bubbleColor = colors[random(0, colors.length - 1)]
-      let cols = 1
-      let rows = 1
-      if (radius * 2 > cellHeight) {
-        rows = 2
-      }
-      if (radius * 2 > cellWidth) {
-        cols = 2
-      }
-      const maxX = cellWidth * cols - radius * 2
-      const maxY = cellHeight * rows - radius * 2
-      const bubblePosInCell = [random(0, maxX, false), random(0, maxY, false)]
-      const boundingOffset = radius
+    const maxX = pivotX + cellWidth * cols - radius * 2
+    const maxY = pivotY + cellHeight * rows - radius * 2
+    const bubblePosInCell = [random(pivotX, maxX, false), random(pivotY, maxY, false)]
+    const boundingOffset = radius
 
-      bubbles.current.push({
-        color: bubbleColor,
-        pos: bubblePosInCell,
-        radius,
-        rows,
-        cols,
-        boundings: {
-          minX: 0 - boundingOffset,
-          maxX: cols * cellWidth - radius * 2 + boundingOffset,
-          maxY: rows * cellHeight - radius * 2 + boundingOffset,
-          minY: 0 - boundingOffset,
-        },
-        fade: random(0.3, 1, false),
-        maxProgress: random(0, 0.8, false),
-      })
+    return {
+      color: bubbleColor,
+      pos: bubblePosInCell,
+      radius,
+      rows,
+      cols,
+      boundaries: {
+        minX: pivotX - boundingOffset,
+        maxX: maxX + boundingOffset,
+        maxY: maxY + boundingOffset,
+        minY: pivotY - boundingOffset,
+      },
+      fade: random(0.3, 0.8, false),
+      maxProgress: random(0, 0.8, false),
     }
   }
-  return [bubbles.current, setShouldInit]
+
+  // fill the grid with bubbles
+  // expect to have 1.5 bubbles per row
+  const prob = 1.5 / cellsPerRow
+  const grids = []
+  let bubbleIndex = 0
+  let bubbles = []
+  let row = 0
+  let col = 0
+
+  while (bubbleIndex < dataSize) {
+    if (Math.random() < prob) {
+      bubbles.push(generateBubble(col * cellWidth, row * cellHeight))
+      bubbleIndex += 1
+    }
+
+    if ((row >= cellsPerCol - 1 && col >= cellsPerRow - 1) || bubbleIndex >= dataSize) {
+      if (bubbles.length > 0) {
+        grids.push({ bubbles, height: cellHeight * (row + 1) })
+      }
+    }
+
+    col += 1
+    if (col >= cellsPerRow) {
+      row += 1
+      col = 0
+    }
+
+    if (row >= cellsPerCol) {
+      bubbles = []
+      row = 0
+      col = 0
+    }
+  }
+  return grids
 }
 
 export default function BubbleTank({
@@ -165,113 +160,84 @@ export default function BubbleTank({
   const tileClasses = useTileStyles()
 
   const dataSize = data.length
-  const [bubbles, setShouldInitBubbles] = useInitBubbles(
-    dataSize,
-    cellHeight,
-    cellsPerRow
-  )
-  const cells = useInitCells(dataSize, cellsPerRow)
-  // const [update, setUpdate] = useState({})
+  const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight })
+  const grids = useMemo(() => initBubbles(dataSize, cellHeight, cellsPerRow), [data, dataSize, cellHeight, cellsPerRow, windowSize])
+
   useEffect(() => {
     function onresize() {
-      // willMountBubble.current = true
-      // need to re-mount bubbles to adjust size
-      setShouldInitBubbles(true)
-      // setUpdate({})
+      if (windowSize.width != window.innerWidth || windowSize.height != window.innerHeight) {
+        setWindowSize({ width: window.innerWidth, height: window.innerHeight })
+      }
     }
     const debounced = debounce(onresize, 100)
     window.addEventListener("resize", debounced)
     return () => {
       window.removeEventListener("resize", debounced)
     }
-  }, [setShouldInitBubbles])
+  }, [setWindowSize])
 
-  const viewportHeight = window.innerHeight
   const unorderedData = useMemo(() => {
     const copy = [...data]
     shuffle(copy)
     return copy
   }, [data])
-  return (
-    <GridList
-      spacing={0}
-      className={classes.gridList}
-      cols={cellsPerRow}
-      cellHeight={cellHeight}
-    >
-      {header && (
-        <GridListTile
-          key="Subheader"
-          cols={cellsPerRow}
-          style={{ height: "auto", margin: "20px" }}
-        >
-          <ListSubheader
-            component="div"
-            style={{
-              textAlign: "center",
-              color: "black",
-              opacity: 1,
-            }}
-          >
-            <h1>{header}</h1>
-          </ListSubheader>
-        </GridListTile>
-      )}
-      {cells.map((bubbleIndex, i) => {
-        let component = null
-        let bubbleProps = null
-        // get the bubble index for each cell
-        if (bubbleIndex >= 0) {
-          bubbleProps = bubbles[bubbleIndex]
-          const bubbleData = unorderedData[bubbleIndex]
-          component = (
-            <Bubble
-              radius={bubbleProps.radius}
-              style={{
-                color: "white",
-                backgroundColor: bubbleProps.color,
-                position: "absolute",
-                top: `${bubbleProps.pos[1]}px`,
-                left: `${bubbleProps.pos[0]}px`,
-              }}
-              boundings={bubbleProps.boundings}
-              title={bubbleData.title}
-              description={bubbleData.description}
-              image={bubbleData.image}
-              links={bubbleData.links}
-              key={bubbleIndex}
-            >
-              {bubbleData.title}
-            </Bubble>
-          )
-        }
 
-        return (
-          <GridListTile
-            classes={tileClasses}
-            key={i}
-            cols={bubbleProps ? bubbleProps.cols : 1}
-            rows={bubbleProps ? bubbleProps.rows : 1}
-          >
-            <ParallaxSection
-              style={{
-                overflow: "visible",
-                pointerEvents: "none",
-                height: "auto",
-                width: "auto",
-              }}
-              triggerHook={0.5}
-              maxProgressValue={
-                bubbleProps ? bubbleProps.maxProgress * viewportHeight : 0
-              }
-              progressUnit="px"
-              fade={bubbleProps ? bubbleProps.fade : 0}
-            >
-              {component}
-            </ParallaxSection>
-          </GridListTile>
-        )
-      })}
-    </GridList>
+  const sections = grids.map((grid, i) => {
+    return (
+      <Section
+        key={`section-${i}`}
+        height="auto"
+      >
+        <div style={{ position: "relative", height: `${grid.height}px` }} >
+          {grid.bubbles.map((bubbleProps, j) => {
+            const bubbleData = unorderedData[j]
+            return (
+              <ParallaxSection
+                style={{
+                  overflow: "visible",
+                  pointerEvents: "none",
+                  height: "auto",
+                  width: "auto",
+                }}
+                className={tileClasses.root}
+                triggerHook={0.5}
+                maxProgressValue={
+                  bubbleProps ? bubbleProps.maxProgress * windowSize.height : 0
+                }
+                progressUnit="px"
+                fade={bubbleProps ? bubbleProps.fade : 0}
+                key={bubbleData.title}
+              >
+                <Bubble
+                  radius={bubbleProps.radius}
+                  style={{
+                    color: "white",
+                    backgroundColor: bubbleProps.color,
+                    position: "absolute",
+                    left: bubbleProps.pos[0],
+                    top: bubbleProps.pos[1],
+                  }}
+                  bounds={bubbleProps.boundaries}
+                  initPos={{ x: bubbleProps.pos[0], y: bubbleProps.pos[1] }}
+                  title={bubbleData.title}
+                  description={bubbleData.description}
+                  image={bubbleData.image}
+                  links={bubbleData.links}
+                // key={bubbleData.title}
+                >
+                  {bubbleData.title}
+                </Bubble>
+              </ParallaxSection>
+            )
+          })}
+        </div>
+      </Section>
+    )
+  })
+
+  return (
+    <div className={classes.gridList}>
+      {sections}
+    </div>
   )
 }
