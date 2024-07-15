3cc9c5c99eb5d1a720b82d4c2225550bd7242cb4
Xian Zhang <xian.z512@gmail.com>
2024-07-14 23:41:58 -0700

    Update isInViewport.js

--- src/utils/isInViewport.js
+++ src/utils/isInViewport.js
@@ -17,12 +17,12 @@ function isInViewport(elem) {
   )
 }
 
-function isBottomInViewport(elem, topMargin = 0, bottomMargin = 0) {
+function isBottomInViewport(elem, outerMargin = 0, innerMargin = 0) {
   if (!elem) return false
   const distance = elem.getBoundingClientRect()
   return (
-    distance.bottom - topMargin > 0 &&
-    distance.bottom + bottomMargin <=
+    distance.bottom - outerMargin > 0 &&
+    distance.bottom + innerMargin <=
       (window.innerHeight || document.documentElement.clientHeight) &&
     distance.left <=
       (window.innerWidth || document.documentElement.clientWidth) &&
@@ -30,12 +30,12 @@ function isBottomInViewport(elem, topMargin = 0, bottomMargin = 0) {
   )
 }
 
-function isTopInViewport(elem, topMargin = 0, bottomMargin = 0) {
+function isTopInViewport(elem, outerMargin = 0, innerMargin = 0) {
   if (!elem) return false
   const distance = elem.getBoundingClientRect()
   return (
-    distance.top - topMargin >= 0 &&
-    distance.top + bottomMargin <
+    distance.top - outerMargin >= 0 &&
+    distance.top + innerMargin <
       (window.innerHeight || document.documentElement.clientHeight) &&
     distance.left <=
       (window.innerWidth || document.documentElement.clientWidth) &&
@@ -59,16 +59,16 @@ function isBelowViewportTop(elem, offset = 1) {
   return distance.top - offset > 0
 }
 
-function isAnyInViewport(elem, margin = 0) {
+function isAnyInViewport(elem, innerMargin = 0) {
   if (!elem) return false
   const distance = elem.getBoundingClientRect()
   return (
     // not the case of top fall below the bottom bounding
-    distance.top + margin <=
+    distance.top + innerMargin <=
       (window.innerHeight || document.documentElement.clientHeight) &&
     distance.left <=
       (window.innerWidth || document.documentElement.clientWidth) &&
-    distance.bottom - margin >= 0 &&
+    distance.bottom - innerMargin >= 0 &&
     distance.right >= 0
   )
 }
