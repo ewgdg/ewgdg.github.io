098d45254712062bb0d913a9956af67d7fd17f7f
Xian Zhang <xianzzz@outlook.com>
2025-07-25 23:57:39 -0700

    refactor: delete tests

--- __tests__/Footer.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { render } from "@testing-library/react"
-import Footer from "../src/components/footer/Footer"
-
-test("Footer renders correctly", () => {
-  const { container } = render(<Footer />)
-  expect(container.firstChild).toMatchSnapshot()
-})
