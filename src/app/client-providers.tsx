0b907e3f5c7ea0ccbc5b1184ae0ccaac8f2f37e7
Xian Zhang <xianzzz@outlook.com>
2026-02-03 23:30:56 -0800

    chore: upgrade MUI to 7.3.7

--- src/app/client-providers.tsx
+++ src/app/client-providers.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import PersistedLayout from '@/components/layouts/persisted-layout'
